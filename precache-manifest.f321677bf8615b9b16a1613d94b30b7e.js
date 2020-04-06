e370055a01a93f9816fa442386870cf5a4f04838
tsharon-byte <sharonova_t@inbox.ru>
2020-04-06 15:34:58 +0300

    Updates

---
+++ precache-manifest.f321677bf8615b9b16a1613d94b30b7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e4ff63d46a0528e2323439352931d51",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "3ec145b4ae85cd02dfdf",
+    "url": "/pomodoro-clock/static/css/main.b433b81c.chunk.css"
+  },
+  {
+    "revision": "377d5341722ff0b1419b",
+    "url": "/pomodoro-clock/static/js/2.2bbe84a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.2bbe84a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ec145b4ae85cd02dfdf",
+    "url": "/pomodoro-clock/static/js/main.562b229a.chunk.js"
+  },
+  {
+    "revision": "c8e805114a65b007e849",
+    "url": "/pomodoro-clock/static/js/runtime-main.8a5682fa.js"
+  }
+]);
